257d4e87ec3adf1dfaf04b70bd781a4ece3575eb
SaboyaDev <saboyadev@gmail.com>
2020-07-26 01:59:26 -0400

    added imagemin webpack plugin to try to optimize the images

--- webpack.prod.js
+++ webpack.prod.js
@@ -6,44 +6,46 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
 
 module.exports = merge(common, {
-  mode: 'production',
-  output: {
-    filename: '[name].[contentHash].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  optimization: {
-    minimizer: [
-      new OptimizeCssAssetsPlugin(),
-      new TerserPlugin(),
-      new HtmlWebpackPlugin({
-        template: './src/index.html',
-        favicon: './src/assets/favicon.png',
-        minify: {
-          removeAttributeQuotes: true,
-          collapseWhitespace: true,
-          removeComments: true,
-        },
-      }),
-    ],
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin({
-      filename: '[name].[contentHash].css',
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          MiniCssExtractPlugin.loader, //3. Extract css into files
-          'css-loader', //2. Turns css into commonjs
-          'sass-loader',
-        ], //1. Turns sass into css
-      },
-    ],
-  },
+	mode: 'production',
+	output: {
+		filename: '[name].[contentHash].bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	optimization: {
+		minimizer: [
+			new OptimizeCssAssetsPlugin(),
+			new TerserPlugin(),
+			new HtmlWebpackPlugin({
+				template: './src/index.html',
+				favicon: './src/assets/favicon.png',
+				minify: {
+					removeAttributeQuotes: true,
+					collapseWhitespace: true,
+					removeComments: true,
+				},
+			}),
+		],
+	},
+	plugins: [
+		new CleanWebpackPlugin(),
+		new MiniCssExtractPlugin({
+			filename: '[name].[contentHash].css',
+		}),
+		new ImageminPlugin({ test: './src/assets/**' }),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: [
+					MiniCssExtractPlugin.loader, //3. Extract css into files
+					'css-loader', //2. Turns css into commonjs
+					'sass-loader',
+				], //1. Turns sass into css
+			},
+		],
+	},
 });
