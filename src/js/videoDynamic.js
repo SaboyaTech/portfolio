dda4de7f85831a9a7b158e4f26c1a6f8e4df50cb
SaboyaDev <saboyadev@gmail.com>
2020-09-08 21:23:48 -0400

    testing mobile background video

--- src/js/videoDynamic.js
+++ src/js/videoDynamic.js
@@ -1,10 +1,10 @@
-window.addEventListener('resize', () => {
-	// console.log(window.innerWidth);
-	if ($(window).width() < 721) {
-		$('#mob').show();
-		$('#desk').hide();
-	} else {
-		$('#mob').hide();
-		$('#desk').show();
-	}
-});
+// window.addEventListener('resize', () => {
+// 	// console.log(window.innerWidth);
+// 	if ($(window).width() < 721) {
+// 		$('#mob').show();
+// 		$('#desk').hide();
+// 	} else {
+// 		$('#mob').hide();
+// 		$('#desk').show();
+// 	}
+// });
