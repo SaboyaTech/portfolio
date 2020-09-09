fe92d8d79e8b572a722112b1913c8bbca6704ad8
SaboyaDev <saboyadev@gmail.com>
2020-09-08 20:57:21 -0400

    added the two video sizes and added the script to handle the window width

---
+++ src/js/videoDynamic.js
@@ -0,0 +1,10 @@
+window.addEventListener('resize', () => {
+	// console.log(window.innerWidth);
+	if ($(window).width() < 721) {
+		$('#mob').show();
+		$('#desk').hide();
+	} else {
+		$('#mob').hide();
+		$('#desk').show();
+	}
+});
