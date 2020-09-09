e97c71ed44bd637db94372bdacb156c684a4fdd6
SaboyaDev <saboyadev@gmail.com>
2020-09-09 00:16:50 -0400

    testing mobile background video

--- src/js/videoDynamic.js
+++ src/js/videoDynamic.js
@@ -1,8 +1,5 @@
-// var videoWidth = 1280;
-// var video = $('#video-wrapper video').attr('srcdesk');
 // if ($(window).width() < 721) {
-// 	videoWidth = 720;
-// 	video = $('.video-wrapper video').attr('srcmob');
+//   $('')
 // }
 // $('.video-wrapper video')
 // 	.attr('width', videoWidth)
