59e175afd7e04c9d378fe8633fafc3a8cd8a3372
SaboyaDev <saboyadev@gmail.com>
2020-11-12 01:38:41 -0500

    deleted the parallax.js, Updated the nav-fx.js Fixed the scrolling up bug with the nav. now it disappears and appears depending whether you are scrolling up or down

--- src/js/nav-fx.js
+++ src/js/nav-fx.js
@@ -2,7 +2,7 @@
 $(window).scroll(function() {
   let height = $(".nav-section").height();
   let scrollTop = $(window).scrollTop();
-  if (scrollTop >= height + 890) {
+  if (scrollTop >= height + 720) {
     // makes the navbar solid
     $(".nav-section").addClass("nav-solid");
   } else {
@@ -13,20 +13,16 @@ $(window).scroll(function() {
 
 /* After a certain point on the page When scrolling down, hide the navbar. When scrolling up, show the navbar */
 // $("#back2Top").hide();
-// var prevScrollpos = window.pageYOffset;
-// window.onscroll = function() {
-//   let scrollTop = $(window).scrollTop();
-//   let currentScrollPos = window.pageYOffset;
-//   if (scrollTop < 790) {
-//     $("#back2Top").hide();
-//   } else if (scrollTop >= 790) {
-//     if (prevScrollpos > currentScrollPos) {
-//       $(".navbar").css("top", "0");
-//       $("#back2Top").fadeOut();
-//     } else {
-//       $(".navbar").css("top", "-8em");
-//       $("#back2Top").fadeIn();
-//     }
-//     prevScrollpos = currentScrollPos;
-//   }
-// };
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  let scrollTop = $(window).scrollTop();
+  let currentScrollPos = window.pageYOffset;
+  if (scrollTop >= 990) {
+    if (prevScrollpos > currentScrollPos) {
+      $(".nav-section").css("top", "0");
+    } else {
+      $(".nav-section").css("top", "-8em");
+    }
+    prevScrollpos = currentScrollPos;
+  }
+};
--- src/js/parallax.js
+++
@@ -1,6 +0,0 @@
-// const parallax = document.querySelector('#contact');
-
-// window.addEventListener('scroll', () => {
-// 	const offset = window.pageYOffset;
-// 	parallax.style.backgroundPositionY = offset * 0.7 + 'px';
-// });
